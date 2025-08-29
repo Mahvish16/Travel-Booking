38b95f3f79f1842455051542385beb1de6a2672c
Mahvish16 <mahvish.ruhi@gmail.com>
2025-08-29 08:02:08 +0530

    Add Login API and improve home page and navbar

--- travelbooking/booking/urls.py
+++ travelbooking/booking/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
-
-    # Auth
     path('register/', views.register, name='register'),
+    path('login/', views.login, name='login')
 ]
--- travelbooking/booking/views.py
+++ travelbooking/booking/views.py
@@ -2,11 +2,10 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate,login as auth_login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 
-def home(request):
-    user = request.user.username
-    return render(request, 'booking/home.html', {'username':user} )
 
 def register(request):
     if request.method == 'POST':
@@ -21,3 +20,22 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'booking/register.html', {'form':form})
 
+def login(request):
+    if request.method == 'POST':
+        username= request.POST["username"]
+        password =  request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request,user)
+            messages.success(request, 'Login successful!')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
+            return render(request, 'booking/login.html')
+    return render(request, 'booking/login.html')
+
+@login_required
+
+def home(request):
+    user = request.user.username
+    return render(request, 'booking/home.html', {'username':user} )
